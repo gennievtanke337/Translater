ec1828979ef3e540e2bdf6ed29c713a08e7b5fa7
sergiy2000000000000000 <sergiykvasniy2009@gmail.com>
2024-06-08 12:03:06 +0300

    test v1

--- func.py
+++ func.py
@@ -1,7 +1,7 @@
 from translate import Translator
 
 yt = input('Напиши що перекласти:')
-
-translator= Translator(to_lang="uk")
+lang = input() #combo box 
+translator= Translator(from_lang='uk',to_lang=lang)
 translation = translator.translate(yt)
 print(translation)
