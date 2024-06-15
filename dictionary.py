bc9429ae60bb810d243a30cbfb74b3625056439b
sergiy2000000000000000 <sergiykvasniy2009@gmail.com>
2024-06-15 12:41:49 +0300

    test 3

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,1 @@
-vocabulary = {'всім': 'to everyone', 'чіл':'relax' ,'бульба':'potato','бульби':'potatoes','бульбу':'potato' }
+vocabulary = {'всім': 'to everyone', 'чіл':'relax' ,'бульба':'potato','бульби':'potatoes','бульбу':'potato','бее': 'ти че баран','MGE': 'МГЕ' }
